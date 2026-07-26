0e95803515ee5ab1247528d5a20ff8847656417b
Ganesh-coder43 <saiganeshkodidela@gmail.com>
2026-07-26 22:55:51 +0530

    Add files via upload

---
+++ celebreties.py
@@ -0,0 +1,435 @@
+fam_person = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 665,
+        "profession": "footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 274,
+        "profession": "cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 282,
+        "profession": "singer",
+        "country": "USA"
+    },
+    {
+        "name": "Narendra Modi",
+        "follower_count": 94,
+        "profession": "Prime Minister",
+        "country": "India"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 507,
+        "profession": "footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 93,
+        "profession": "actress",
+        "country": "India"
+    },
+    {
+        "name": "The Rock",
+        "follower_count": 394,
+        "profession": "actor and wrestler",
+        "country": "USA"
+    },
+    {
+        "name": "Alia Bhatt",
+        "follower_count": 86,
+        "profession": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 392,
+        "profession": "businesswoman",
+        "country": "USA"
+    },
+    {
+        "name": "Deepika Padukone",
+        "follower_count": 80,
+        "profession": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Beyonce",
+        "follower_count": 311,
+        "profession": "singer",
+        "country": "USA"
+    },
+    {
+        "name": "Salman Khan",
+        "follower_count": 72,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 294,
+        "profession": "singer",
+        "country": "Canada"
+    },
+    {
+        "name": "MS Dhoni",
+        "follower_count": 50,
+        "profession": "cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 375,
+        "profession": "singer",
+        "country": "USA"
+    },
+    {
+        "name": "Rohit Sharma",
+        "follower_count": 46,
+        "profession": "cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Robert Downey Jr.",
+        "follower_count": 57,
+        "profession": "actor",
+        "country": "USA"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 49,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Neymar Jr",
+        "follower_count": 231,
+        "profession": "footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Ratan Tata",
+        "follower_count": 14,
+        "profession": "industrialist",
+        "country": "India"
+    },
+    {
+        "name": "LeBron James",
+        "follower_count": 160,
+        "profession": "basketball player",
+        "country": "USA"
+    },
+    {
+        "name": "Hardik Pandya",
+        "follower_count": 39,
+        "profession": "cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 124,
+        "profession": "singer",
+        "country": "USA"
+    },
+    {
+        "name": "Anand Mahindra",
+        "follower_count": 11,
+        "profession": "businessman",
+        "country": "India"
+    },
+    {
+        "name": "Kylian Mbappe",
+        "follower_count": 126,
+        "profession": "footballer",
+        "country": "France"
+    },
+    {
+        "name": "Neeraj Chopra",
+        "follower_count": 10,
+        "profession": "javelin thrower",
+        "country": "India"
+    },
+    {
+        "name": "MrBeast",
+        "follower_count": 72,
+        "profession": "YouTuber",
+        "country": "USA"
+    },
+    {
+        "name": "Sachin Tendulkar",
+        "follower_count": 50,
+        "profession": "cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 73,
+        "profession": "actress",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Kapil Sharma",
+        "follower_count": 48,
+        "profession": "comedian",
+        "country": "India"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 178,
+        "profession": "actress",
+        "country": "USA"
+    },
+    {
+        "name": "Rashmika Mandanna",
+        "follower_count": 46,
+        "profession": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Elon Musk",
+        "follower_count": 8,
+        "profession": "businessman",
+        "country": "USA"
+    },
+    {
+        "name": "Mukesh Ambani",
+        "follower_count": 2,
+        "profession": "businessman",
+        "country": "India"
+    },
+    {
+        "name": "Tom Holland",
+        "follower_count": 66,
+        "profession": "actor",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Yogi Adityanath",
+        "follower_count": 18,
+        "profession": "politician",
+        "country": "India"
+    },
+    {
+        "name": "Mike Tyson",
+        "follower_count": 33,
+        "profession": "boxer",
+        "country": "USA"
+    },
+    {
+        "name": "Allu Arjun",
+        "follower_count": 30,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Sadhguru",
+        "follower_count": 14,
+        "profession": "spiritual leader",
+        "country": "India"
+    },
+    {
+        "name": "Hrithik Roshan",
+        "follower_count": 49,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+        "name": "PV Sindhu",
+        "follower_count": 4,
+        "profession": "badminton player",
+        "country": "India"
+    },
+    {
+        "name": "Ranveer Singh",
+        "follower_count": 48,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Shreya Ghoshal",
+        "follower_count": 32,
+        "profession": "singer",
+        "country": "India"
+    },
+    {
+        "name": "Kriti Sanon",
+        "follower_count": 59,
+        "profession": "actress",
+        "country": "India"
+    },
+ 
+    {
+        "name": "Nikhil Kamath",
+        "follower_count": 2,
+        "profession": "entrepreneur",
+        "country": "India"
+    },
+    {
+        "name": "Rahul Gandhi",
+        "follower_count": 10,
+        "profession": "politician",
+        "country": "India"
+    },
+    {
+        "name": "Viswanathan Anand",
+        "follower_count": 1,
+        "profession": "chess grandmaster",
+        "country": "India"
+    },
+    {
+        "name": "Diljit Dosanjh",
+        "follower_count": 23,
+        "profession": "singer and actor",
+        "country": "India"
+    },
+    {
+        "name": "Ram Charan",
+        "follower_count": 24,
+        "profession": "actor",
+        "country": "India"
+    },
+    {
+    "name": "Sundar Pichai",
+    "follower_count": 3,
+    "profession": "CEO of Google",
+    "country": "India"
+},
+{
+    "name": "Satya Nadella",
+    "follower_count": 2,
+    "profession": "CEO of Microsoft",
+    "country": "India"
+},
+{
+    "name": "CarryMinati",
+    "follower_count": 21,
+    "profession": "YouTuber",
+    "country": "India"
+},
+{
+    "name": "Bhuvan Bam",
+    "follower_count": 20,
+    "profession": "YouTuber",
+    "country": "India"
+},
+{
+    "name": "Ashish Chanchlani",
+    "follower_count": 17,
+    "profession": "YouTuber",
+    "country": "India"
+},
+{
+    "name": "Dhruv Rathee",
+    "follower_count": 15,
+    "profession": "YouTuber",
+    "country": "India"
+},
+{
+    "name": "PewDiePie",
+    "follower_count": 21,
+    "profession": "YouTuber",
+    "country": "Sweden"
+},
+{
+    "name": "Mark Zuckerberg",
+    "follower_count": 14,
+    "profession": "businessman",
+    "country": "USA"
+},
+{
+    "name": "Jeff Bezos",
+    "follower_count": 5,
+    "profession": "businessman",
+    "country": "USA"
+},
+{
+    "name": "Tim Cook",
+    "follower_count": 2,
+    "profession": "CEO of Apple",
+    "country": "USA"
+},
+{
+    "name": "Sam Altman",
+    "follower_count": 4,
+    "profession": "entrepreneur",
+    "country": "USA"
+},
+{
+    "name": "Jensen Huang",
+    "follower_count": 2,
+    "profession": "CEO of NVIDIA",
+    "country": "USA"
+},
+{
+    "name": "Novak Djokovic",
+    "follower_count": 16,
+    "profession": "tennis player",
+    "country": "Serbia"
+},
+{
+    "name": "Rafael Nadal",
+    "follower_count": 22,
+    "profession": "tennis player",
+    "country": "Spain"
+},
+{
+    "name": "Roger Federer",
+    "follower_count": 13,
+    "profession": "tennis player",
+    "country": "Switzerland"
+},
+{
+    "name": "Usain Bolt",
+    "follower_count": 15,
+    "profession": "sprinter",
+    "country": "Jamaica"
+},
+{
+    "name": "Stephen Curry",
+    "follower_count": 59,
+    "profession": "basketball player",
+    "country": "USA"
+},
+{
+    "name": "Conor McGregor",
+    "follower_count": 47,
+    "profession": "MMA fighter",
+    "country": "Ireland"
+},
+{
+    "name": "Khabib Nurmagomedov",
+    "follower_count": 40,
+    "profession": "MMA fighter",
+    "country": "Russia"
+},
+{
+    "name": "Keanu Reeves",
+    "follower_count": 14,
+    "profession": "actor",
+    "country": "Canada"
+},
+{
+    "name": "Will Smith",
+    "follower_count": 69,
+    "profession": "actor",
+    "country": "USA"
+},
+{
+    "name": "Johnny Depp",
+    "follower_count": 30,
+    "profession": "actor",
+    "country": "USA"
+},
+{
+    "name": "Jackie Chan",
+    "follower_count": 12,
+    "profession": "actor",
+    "country": "Hong Kong"
+}
+]
